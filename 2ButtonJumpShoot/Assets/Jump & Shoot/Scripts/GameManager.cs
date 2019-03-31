6aa1dd9dac2b8f4e9a0c9df3876a3b9188ac5c73
Daniel <arctic-crow@web.de>
2019-03-31 20:02:26 +0200

    Imported Jump and Shoot Template

---
+++ 2ButtonJumpShoot/Assets/Jump & Shoot/Scripts/GameManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    public TextMeshProUGUI scoreText;
+
+    public TextMeshProUGUI bestValueText;
+    public TextMeshProUGUI bestText;
+
+    public GameObject GameOverPanel;
+    public GameObject GameOverEffectPanel;
+
+    public GameObject StartEffectPanel;
+
+    public GameObject HowToPlayPanel;
+
+    [HideInInspector]
+    public bool isDead;
+
+
+    int score = 0;
+
+    void Awake()
+    {
+        isDead = false;
+        Application.targetFrameRate = 60;
+        Time.timeScale = 1.0f;
+    }
+
+    void Start()
+    {
+        StartCoroutine(StartEffect());
+    }
+
+
+    IEnumerator StartEffect()
+    {
+        StartEffectPanel.SetActive(true);
+        yield return new WaitForSecondsRealtime(0.5f);
+        StartEffectPanel.SetActive(false);
+        yield break;
+    }
+
+
+    public void AddScore(int value)
+    {
+        score += value;
+        scoreText.text = score.ToString();
+
+        if (score > PlayerPrefs.GetInt("BestScore", 0))
+        {
+            bestValueText.text = score.ToString();
+            PlayerPrefs.SetInt("BestScore", score);
+        }
+    }
+
+
+    public void GameOver()
+    {
+        isDead = true;
+        StartCoroutine(GameOverCoroutine());
+    }
+
+    IEnumerator GameOverCoroutine()
+    {
+        Time.timeScale = 0.1f;
+        GameOverEffectPanel.SetActive(true);
+        scoreText.color = Color.white;
+        bestText.color = Color.gray;
+        bestValueText.color = Color.gray;
+
+        yield return new WaitForSecondsRealtime(0.5f);
+        Time.timeScale = 0.02f;
+        GameOverPanel.SetActive(true);
+
+        yield return new WaitForSecondsRealtime(2.5f);
+        Time.timeScale = 0f;
+
+
+        yield break;
+    }
+
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+
+    public void OpenHowToPanel(){
+        HowToPlayPanel.SetActive(true);
+    } 
+    
+    public void CloseHowToPanel(){
+         HowToPlayPanel.SetActive(false);
+    }
+
+
+}
