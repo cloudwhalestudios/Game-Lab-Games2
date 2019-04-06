79acc713bf8de90deef79e9ca76bf831d351b798
Daniel <arctic-crow@web.de>
2019-04-06 18:09:02 +0200

    Bug Fixes (no more player disappearing hopefully)

--- 2ButtonJumpShoot/Assets/Scripts/MainMenuSceneController.cs
+++ 2ButtonJumpShoot/Assets/Scripts/MainMenuSceneController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuSceneManager : MonoBehaviour
+public class MainMenuSceneController : MonoBehaviour
 {
     private void Start()
     {
--- 2ButtonJumpShoot/Assets/Scripts/MainSceneController.cs
+++ 2ButtonJumpShoot/Assets/Scripts/MainSceneController.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainSceneManager : MonoBehaviour
+public class MainSceneController : MonoBehaviour
 {
     GameObject player;
 
