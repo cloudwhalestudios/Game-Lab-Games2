d13cb2ff118942a351188cf8ef1e5245eb5cad69
ArcticCrow <arctic-crow@web.de>
2019-04-08 11:06:21 +0200

    DemoDay Wave Game

---
+++ 2ButtonWaveGameDemoDay/Assets/Wave/Scripts/GameManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    int score = 0;
+    public TextMeshProUGUI CurrentScoreTextTMPro;
+    public TextMeshProUGUI BestScoreTextTMPro;
+
+    public GameObject GameOverPanel;
+    public GameObject GameOverEffectPanel;
+
+    public GameObject touchToMoveTextObj;
+
+    public GameObject StartFadeInObj;
+
+    static int PlayCount;
+
+
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+
+
+        Time.timeScale = 1.0f;
+
+
+        BestScoreTextTMPro.text = PlayerPrefs.GetInt("BestScore", 0).ToString();
+        StartCoroutine(FadeIn());
+    }
+
+    void Update()
+    {
+        if (touchToMoveTextObj.activeSelf == false) return;
+        if (Input.GetMouseButton(0))
+        {
+            touchToMoveTextObj.SetActive(false);
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        StartFadeInObj.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        StartFadeInObj.SetActive(false);
+        yield break;
+    }
+
+
+    
+
+    public void addScore()
+    {
+        score++;
+        CurrentScoreTextTMPro.text = score.ToString();
+
+        if (score > PlayerPrefs.GetInt("BestScore", 0))
+        {
+            PlayerPrefs.SetInt("BestScore", score);
+            BestScoreTextTMPro.text = PlayerPrefs.GetInt("BestScore", 0).ToString();
+        }
+    }
+
+
+    public void Gameover()
+    {
+        StartCoroutine(GameoverCoroutine());
+    }
+
+
+    IEnumerator GameoverCoroutine()
+    {
+        GameOverEffectPanel.SetActive(true);
+        Time.timeScale = 0.1f;
+        yield return new WaitForSecondsRealtime(0.5f);
+        GameOverPanel.SetActive(true);
+        yield break;
+    }
+
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
